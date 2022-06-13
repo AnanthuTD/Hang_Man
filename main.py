5d5e491b723ed3ded478817e198a7d6d368b4903
AnanthuTD <ananthutd2021@gmail.com>
2022-06-13 23:12:04 +0530

    button a created . can pass guess to Game.py . need to stop looping in Game.py which was previously used for inputting the guess and for checking the win.

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from tkinter import *
+import tkinter as ttk
 
 from squarcle import *
-from RandomWord import random_word
 
-window = Tk()
+from tkinter.ttk import *
 
-canvas = Canvas(window, bg="#121212", bd=0, highlightthickness=0, width=900, height=300)
+from Game import *
+
+hm = HangMan
+
+window = ttk.Tk()
+
+canvas = ttk.Canvas(window, bg="#121212", bd=0, highlightthickness=0, width=900, height=300)
 textBox = round_rectangle(0, 50, 900, 200, 'grey', canvas)
 canvas.pack()
 
@@ -19,64 +24,9 @@ posRight = int(window.winfo_screenwidth() / 2 - winWidth / 2)
 posDown = int(window.winfo_screenheight() / 2 - winHeight / 2)
 window.geometry("{}x{}+{}+{}".format(winWidth, winHeight, posRight, posDown))
 
-# packing
+DispLabel = ttk.Label(window, text='Are you a Geek?', font=('italic', 30), bg='grey', fg='white')
+DispLabel.place(y=120, relx=0.5, anchor='center')
 
-
-print('\033[1m \033[92m-------------------------------------------------------------------\033[92m\n'
-      '\n'
-      '    \033[96m                       HANGMAN                     \033[96m            \n'
-      '\n'
-      '\033[92m--------------------------------------------------------------------\033[0m\n'
-      )
-
-choice = 'y'
-
-while choice == 'y':
-    # word = random.choice(lst)  # SELECTING A RANDOM WORD FROM word_list.py
-    word = str(random_word())
-    life = j = win = count = 0
-    print(word)
-
-    ges_word = []
-    entered_letter = []
-    list_ = 1
-
-    for i in word:
-        if i.isalpha():
-            ges_word.append('_')  # (_ _ _ ......)
-            j += 1  # LENGTH OF STRING
-
-    life = 5  # total five life left
-    print("\nLET'S START\n")
-    while life != 0 and j != win:
-        print(*ges_word)
-
-        label = Label(window, text=ges_word)
-        label.pack()
-
-        guess = input("\nWhat is your guess : ")
-        while guess in ges_word:
-            guess = input("\nYou have already guessed this letter ! Try another letter : ")
-        for char in range(j):
-            # to check all elements in word
-            if guess.lower() in word[char]:  # checking our guess is
-                ges_word[char] = word[char]  # assigning the correct word to the char th position of ges_word
-                win += 1  # counting number of wins
-                label.pack_forget()
-
-        if win == count:  # if guess is wrong
-            life -= 1
-            print(f"INCORRECT \033[91m{life} life's left\033[0m ")
-        else:
-            print('\033[92m you got it right \033[0m')
-
-        count = int(win)  # assigning number of wins to count
-
-    if life == 0:
-        print('\nsorry you have used all of you life !\n\nThe word was : ' + word + '\n Better luck next time .')
-    else:
-        print(*ges_word, '\nhurry ! , you have guessed the word right\n\033[95m\033[1m YOU WON!\033[0m')
-        label.pack()
-
-    choice = input('\nDo you want to play again (y/n) :')
+aButton = Button(window, text="A", command=lambda: hm.hang_man(guess=aButton['text']))
+aButton.pack()
 window.mainloop()
